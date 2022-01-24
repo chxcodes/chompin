ae393e687e490121a7ce70b67d610ef673dd0e22
Chris Hyle <chx.codes@gmail.com>
2022-01-24 00:32:29 -0800

    Fix env

--- pages/api/createTokenRequest.ts
+++ pages/api/createTokenRequest.ts
@@ -1,7 +1,15 @@
 import Ably from "ably/promises";
+import { NextApiRequest, NextApiResponse } from "next";
+import { env } from "process";
 
-export default async function handler(req, res) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'chompin' });
-    res.status(200).json(tokenRequestData);
-};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (!env.ABLY_API_KEY) throw new Error("Mis-configured key: ABLY_API_KEY");
+  const client = new Ably.Realtime(env.ABLY_API_KEY);
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: "chompin",
+  });
+  res.status(200).json(tokenRequestData);
+}
